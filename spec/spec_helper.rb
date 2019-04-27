886b676d4eb8ce5d85664bc2e659e7b28a2208f7
Jon Mohrbacher <johnnymo87@gmail.com>
2019-04-27 10:07:25 -0400

    Make sure tests fail when threads throw exceptions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,11 @@
 require 'coveralls'
 Coveralls.wear!
 
+# https://stackoverflow.com/a/9095369/10513533
+# Without this, exceptions coming from code run in threads won't fail tests.
+# For example, WebSocketVCR::RecordableWebsocketClient uses threads.
+Thread.abort_on_exception = true
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.order = 'random'
